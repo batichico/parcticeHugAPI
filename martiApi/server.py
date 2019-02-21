c7fcfd44dce1713944d472c171e984aae6b79b68
Samuel Guibert <samu4a@gmail.com>
2019-02-21 01:34:47 +0100

    Update server.py

--- martiApi/server.py
+++ martiApi/server.py
@@ -5,9 +5,7 @@ import apiLogic
 def getTopics(padre):
     return apiLogic.get_botones(padre)
 
-
 @hug.get('/videojuegos')
 def getVideojuegos():
-
     videojuegos = apiLogic.get_botones("0")
     return (videojuegos)
